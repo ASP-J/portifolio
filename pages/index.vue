023668f668040c6b90cbc1143e17164358ebdfe7
ASP-J <jmigotrski@gmail.com>
2024-12-07 20:16:29 -0300

    first commit

---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Welcome to My Portfolio</h1>
+    <p>This is the index page of my portfolio.</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+// You can add your script logic here
+</script>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
